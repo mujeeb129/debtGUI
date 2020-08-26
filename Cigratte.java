fe208027b31c043f2ba59cfe240b7a726d0b42db
Mujeeb Rahman <mujeebmj313@gmail.com>
2020-08-26 18:06:14 +0530

    Variable names initialised and declared

---
+++ Cigratte.java
@@ -0,0 +1,18 @@
+class Cigratte{
+
+	int Scissors = 8;
+	int Gold = 10;
+	int MiniGold = 6;
+	int Kings = 17;
+	int EssayLights = 10;
+	int EssayLightsBox = 110;
+	int bigWills = 11;
+	int mediumWills = 8;
+	int miniWills = 5;
+
+
+
+	public static void main(String[] args){
+		System.out.println("This is going to be my project");
+	}
+}
