2e35a87fcbf44e9a890f5bb9d76c199535abc1dd
Mujeeb Rahman <mujeebmj313@gmail.com>
2020-09-17 00:06:07 +0530

    Added some functions and did some crud operation settings

--- Main.py
+++ Main.py
@@ -1,18 +1,73 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import database as db
+import time
 
+data = db.SetUp('debtbook.db')
+
+additions = ""
+additionList = []
+cigarDict = {'King - 17': 17 ,
+			 'Gold - 10': 10 ,
+			 'Mini Gold - 6' : 6 ,
+			 'Essay - 10' : 10 ,
+			 'Essay Pack - 110' : 110 ,
+			 'Scissors - 8' : 8 , 
+			 'Wills - 11' : 11 ,
+			 'M Wills - 8' : 8 ,
+			 'Mini Wills - 5' : 5 }
+
+def populate():
+	global additions , cigarDict , additionList
+	cigarname = cigarName.get()
+	amountS = amount.get()
+	rate = cigarDict[cigarname]
+	if cigarname != '':
+		if additions == '' :
+			additions = additions + str(rate * int(amountS))
+		else :
+			additions = additions + ' + ' + str(rate * int(amountS))
+		calculate.set(additions)
+		additionList.append(rate * int(amountS))
+		print(additionList)
+	else :
+		messagebox.showerror('Please fill' , 'Please fill all the fields')
+
+
+def populateList():
+	listBox.delete(0 , END)
+	for row in data.fetch():
+		listBox.insert(END , row)
+
+def save():
+	global additionList
+	total = sum(additionList)
+	nameS = name.get()
+	amountS = amount.get()
+	instanceS = time.ctime()
+	if nameS != '' or amountS != '' :
+		data.insert(nameS , total , instanceS)
+
+	else :
+		messagebox.showerror('Please fill' , 'Please fill all the fields')
+	populateList()
+
+
+def delete():
 
 
 window = Tk()
 window.title('Debt app')
-window.geometry('500x500+100+100')
+window.geometry('500x300+100+100')
+window.configure(bg = '#FFFFFF')
 
-mainFrame = Frame(window)
+mainFrame = Frame(window , bg = '#FFFFFF')
 mainFrame.grid()
 #Name variable to store name from the entry label
 name = StringVar()
 #Name entry and label
-nameLabel = Label(mainFrame , text = 'Name')
+nameLabel = Label(mainFrame , text = 'Name' , bg = '#FFFFFF')
 nameLabel.grid(row = 0 , column = 0)
 
 nameEntry = Entry(mainFrame , textvariable = name)
@@ -20,29 +75,50 @@ nameEntry.grid(row = 0 , column = 1)
 #Amount variable to input the number of cigars
 amount = StringVar()
 #Amount entry and cigar type combobox
-amountLabel = Label(mainFrame , text = 'Amount')
+amountLabel = Label(mainFrame , text = 'Amount' , bg = '#FFFFFF')
 amountLabel.grid(row = 1 , column = 0)
 
 amountEntry = Entry(mainFrame , textvariable = amount)
 amountEntry.grid(row = 1 , column = 1)
 
-cigarCombobox = ttk.Combobox(mainFrame)
+cigarName = StringVar()
+cigarCombobox = ttk.Combobox(mainFrame , state = 'readonly' , textvariable = cigarName)
 cigarCombobox.grid(row = 1 , column = 2)
+
+cigarCombobox['values'] = ('King - 17' ,
+						   'Gold - 10' ,
+						   'Mini Gold - 6' ,
+						   'Essay - 10' ,
+						   'Essay Pack - 110' ,
+						   'Scissors - 8' , 
+						   'Wills - 11' ,
+						   'M Wills - 8' ,
+						   'Mini Wills - 5')
+
+scrollbar = Scrollbar(mainFrame)
+listBox.configure(yviewcommand = scrollbar.set)
+scrollbar.configure(command = listBox.yview)
+
 #Add button to the cigar
-addButton = Button(mainFrame , text = 'Add')
-addButton.grid(row = 1 , column = 9)
+addButton = Button(mainFrame , text = 'Add' , command = populate , bg = '#FFFFFF' , bd = 0)
+addButton.grid(row = 1 , column = 9 , sticky = 'E')
 #Calculation variable 
 calculate = StringVar()
 #Calculation display and save button
-calculationListbox = Listbox(mainFrame , height = 1 , width  = 50 ,)
-calculationListbox.grid(row = 2 , column = 0 , columnspan = 8 , )
+calculationEntry = Entry(mainFrame , textvariable = calculate)
+calculationEntry.grid(row = 2 , column = 0 , columnspan = 8 , )
 
-saveButton = Button(mainFrame , text = 'Save')
-saveButton.grid(row = 2 , column = 9)
+
+saveButton = Button(mainFrame , text = 'Save' , command = save , bg = '#FFFFFF' , bd = 0)
+saveButton.grid(row = 2 , column = 9 , sticky = 'E')
 
 listBox = Listbox(mainFrame , width = 50)
 listBox.grid(row = 3 , column =  0 , columnspan = 8 , rowspan = 6)
 
+deleteButton = Button(mainFrame , text = 'Delete' , bd = 0 , bg = '#FFFFFF')
+deleteButton.grid(row = 3 , column = 9)
+
+updateButton = Button(mainFrame , text = 'Update' , bd = 0 , bg = '#FFFFFF')
 
 
 
@@ -50,5 +126,5 @@ listBox.grid(row = 3 , column =  0 , columnspan = 8 , rowspan = 6)
 
 
 
-
+populateList()
 window.mainloop()
--- database.py
+++ database.py
@@ -5,32 +5,32 @@ class SetUp:
 	def __init__(self , db):
 		self.conn = sqlite3.connect(db)
 		self.c = self.conn.cursor()
-		self.c.execute("""CREATE TABLE IF NOT EXIST debtbox(
+		self.c.execute("""CREATE TABLE IF NOT EXISTS debtbox(
 						  id INTEGER PRIMARY KEY ,
 						  name TEXT ,
 						  amount INTEGER ,
 						  instance TEXT)""")
 		self.conn.commit()
 
-		def fetch(self):
-			self.c.execute("SELECT * FROM debtbox")
-			rows = self.conn.fetchall()
-			self.conn.commit()
-			return rows
+	def fetch(self):
+		self.c.execute("SELECT * FROM debtbox")
+		rows = self.c.fetchall()
+		self.conn.commit()
+		return rows
 
-		def insert(self , name , amount , instance):
-			self.c.execute("INSERT INTO debtbox VALUES(NULL , ? , ? , ?)",(name , amount , instance))
-			self.conn.commit()
+	def insert(self , name , amount , instance):
+		self.c.execute("INSERT INTO debtbox VALUES(NULL , ? , ? , ?)",(name , amount , instance))
+		self.conn.commit()
 
-		def update(self ,id , name , amount , instance):
-			self.c.execute("""UPDATE debtbox
-							  SET name = ? , amount = ? , instance = ?
-							  WHERE id = ?""",(name , amount , instance , id))
-			self.conn.commit()
+	def update(self ,id , name , amount , instance):
+		self.c.execute("""UPDATE debtbox
+						  SET name = ? , amount = ? , instance = ?
+						  WHERE id = ?""",(name , amount , instance , id))
+		self.conn.commit()
 
-		def delete(self , id):
-			self.c.execute("DELETE FROM debtbox WHERE id = ?",(id,))
-			self.conn.commit()
+	def delete(self , id):
+		self.c.execute("DELETE FROM debtbox WHERE id = ?",(id,))
+		self.conn.commit()
 
-		def __del__(self):
-			self.conn.close()			
+	def __del__(self):
+		self.conn.close()			
